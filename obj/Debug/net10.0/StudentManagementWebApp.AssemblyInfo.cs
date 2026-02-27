a502857bd444a3a52adfc7b8f8770d27507a0472
Khethelo Mnyandu <mnyandukhethelo@gmail.com>
2026-02-27 14:54:24 +0200

    commit to main

--- obj/Debug/net10.0/StudentManagementWebApp.AssemblyInfo.cs
+++ obj/Debug/net10.0/StudentManagementWebApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StudentManagementWebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+508373c0f1980dd92553280118cfe6ad18565ca6")]
 [assembly: System.Reflection.AssemblyProductAttribute("StudentManagementWebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StudentManagementWebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
